6ca89b0f3d7099e3abc678f370d9c22c9ddf3dda
afsarequebal <afsarequebal@gmail.com>
2022-03-20 12:35:53 -0400

    New Code

--- AlgoAndDS/src/Distinct_Subsequences_115.java
+++ AlgoAndDS/src/Distinct_Subsequences_115.java
@@ -1,4 +1,13 @@
 
 public class Distinct_Subsequences_115 {
+  public static void main(String[] args) {
+    int num = numDistinct("rabbbit", "rabbit");
+    System.out.println(num);
+  }
 
+  public static int numDistinct(String s, String t) {
+    int ret = 0 ; 
+    
+    return ret;
+  }
 }
